5206822e55d6c71bff9a5b23c6928b7fd12f82d5
Illia Teliuk <illiateliuk@gmail.com>
2023-09-25 23:19:15 +0300

    Finished the tutorial.

--- js/main.js
+++ js/main.js
@@ -14,26 +14,42 @@ var swiper = new Swiper(".popular-content", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  breakpoints:{
+  breakpoints: {
     280: {
-        slidesPerView: 1,
-        spaceBetween: 10,
+      slidesPerView: 1,
+      spaceBetween: 10,
     },
     320: {
-        slidesPerView: 2,
-        spaceBetween: 10,
+      slidesPerView: 2,
+      spaceBetween: 10,
     },
     510: {
-        slidesPerView: 2,
-        spaceBetween: 10,
+      slidesPerView: 2,
+      spaceBetween: 10,
     },
     758: {
-        slidesPerView: 3,
-        spaceBetween: 10,
+      slidesPerView: 3,
+      spaceBetween: 10,
     },
     900: {
-        slidesPerView: 4,
-        spaceBetween: 20,
+      slidesPerView: 4,
+      spaceBetween: 20,
     }
   }
 });
+//Show Video
+let playBtn = document.querySelector(".play-movie");
+let video = document.querySelector(".video-container");
+let myVideo = document.querySelector("#myVideo");
+let closeBtn = document.querySelector(".close-video");
+
+playBtn.onclick = () => {
+  video.classList.add('show-video');
+  //Auto Play when click on Button
+  myVideo.play();
+}
+closeBtn.onclick = () => {
+  video.classList.remove('show-video');
+  //Pause the video when click on Button
+  myVideo.pause();
+}
